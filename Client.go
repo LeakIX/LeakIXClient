0578f688d0ef15a6674407de2354a4849091af0d
Gregory Boddin <gregory@siwhine.net>
2020-07-15 14:47:35 +0200

    Easy to use golang client

---
+++ Client.go
@@ -0,0 +1,79 @@
+package LeakIXClient
+
+import (
+	"encoding/json"
+	"fmt"
+	"gitlab.nobody.run/tbi/core"
+	"io/ioutil"
+	"log"
+	"net/http"
+	url2 "net/url"
+	"time"
+)
+
+var LeakIXProxy = &core.ProxiedPlugin{}
+var LeakIXHttpTranport = &http.Transport{
+	DialContext:           LeakIXProxy.DialContext,
+	ResponseHeaderTimeout: 5 * time.Second,
+	ExpectContinueTimeout: 5 * time.Second,
+}
+var HttpClient = &http.Client{
+	Transport: LeakIXHttpTranport,
+	Timeout:   5 * time.Second,
+}
+
+type SearchResultsClient struct {
+	Scope         string
+	Query         string
+	SearchResults []*SearchResult
+	Position      int
+	Page          int
+}
+
+func (sc *SearchResultsClient) Next() bool {
+	if len(sc.SearchResults) > sc.Position {
+		sc.Position++
+		return true
+	}
+	// Try to load next page
+	results, _ := GetSearchResults(sc.Scope, sc.Query, sc.Page)
+	for _, result := range results {
+		sc.SearchResults = append(sc.SearchResults, result)
+	}
+	sc.Page++
+	if len(sc.SearchResults) > sc.Position {
+		sc.Position++
+		return true
+	}
+	return false
+}
+
+func (sc *SearchResultsClient) SearchResult() *SearchResult {
+	return sc.SearchResults[sc.Position-1]
+}
+
+func GetSearchResults(scope string, query string, page int) ([]*SearchResult, error) {
+	url := fmt.Sprintf(
+		"https://leakix.net/search?scope=%s&q=%s&page=%d", url2.QueryEscape(scope), url2.QueryEscape(query), page)
+	log.Println(url)
+	var searchResults []*SearchResult
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Accept", "application/json")
+	resp, err := HttpClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return searchResults, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return searchResults, err
+	}
+	err = json.Unmarshal(body, &searchResults)
+	if err != nil {
+		log.Println(err)
+		return searchResults, err
+	}
+	return searchResults, nil
+}
---
+++ SearchResult.go
@@ -0,0 +1,56 @@
+package LeakIXClient
+
+import (
+	"time"
+)
+
+// Single leak occurence on a single service
+type SearchResult struct {
+	Ip          string                    `json:"ip"`
+	Port        string                    `json:"port"`
+	Type        string                    `json:"type"`
+	Time        time.Time                 `json:"time"`
+	Date        int64                     `json:"date"`
+	Data        string                    `json:"data"`
+	Headers     map[string][]string       `json:"headers"`
+	Plugin      string                    `json:"plugin"`
+	Network     Network                   `json:"network"`
+	GeoLocation GeoLocation               `json:"geoip"`
+	Credentials []*HostServiceCredentials `json:"credentials"`
+	Software    Software                  `json:"software"`
+}
+
+type HostServiceCredentials struct {
+	NoAuth   bool   `json:"noauth"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Key      string `json:"key"`
+	Raw      []byte `json:"raw"`
+}
+
+type Network struct {
+	OrganisationName string `json:"organization_name"`
+	ASN              int    `json:"asn"`
+}
+
+type GeoLocation struct {
+	ContinentName  string   `json:"continent_name"`
+	RegionISOCode  string   `json:"region_iso_code"`
+	CityName       string   `json:"city_name"`
+	CountryISOCode string   `json:"country_iso_code"`
+	CountryName    string   `json:"country_name"`
+	RegionName     string   `json:"region_name"`
+	GeoPoint       GeoPoint `json:"location"`
+}
+
+type GeoPoint struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
+}
+
+type Software struct {
+	Name            string     `json:"name"`
+	Version         string     `json:"version"`
+	OperatingSystem string     `json:"os"`
+	Modules         []Software `json:"modules"`
+}
