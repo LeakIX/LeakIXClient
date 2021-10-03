c121130057834eba400bbdb8a62e6e452487540b
Gregory Boddin <gregory@siwhine.net>
2021-10-03 21:38:56 +0200

    Preparing for v2 release, using l9format

--- example/main.go
+++ example/main.go
@@ -8,12 +8,12 @@ func main(){
 	// Create a searcher
 	LeakIXSearch := LeakIXClient.SearchResultsClient{
 		Scope: "leak",
-		Query: "protocol:kafka AND \"telecom_italia_data\"",
+		Query: "+protocol:elasticsearch  +\"telecom_italia_data\"",
 	}
 	// Iterate, the lib will query further pages if needed
 	for LeakIXSearch.Next() {
 		// Use the result
 		leak := LeakIXSearch.SearchResult()
-		fmt.Printf("%s:%s - Country:%s\n", leak.Ip, leak.Port, leak.GeoLocation.CountryName)
+		fmt.Printf("%s:%s - Country:%s\n", leak.Ip, leak.Port, leak.GeoIp.CountryName)
 	}
 }
